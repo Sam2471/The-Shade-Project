bc329b57d9ea5ff640a24f120fd8e758c267a957
Samuel Flowers <475197@chrome.escg.ac.uk>
2020-11-16 20:08:47 +0000

    Added flashlight and sound cue scripts

---
+++ Assets/Main Assets/Scripts/Flashlight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public Light flashlight;
+    public AudioSource flashlightsource;
+    public AudioClip toggleon;
+    public AudioClip toggleoff;
+
+
+    private bool isactive;
+   
+    void Start()
+    {
+        isactive = true;
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (isactive == false)
+            {
+                flashlight.enabled = true;
+                isactive = true;
+                flashlightsource.PlayOneShot(toggleon);
+            }
+            else if (isactive == true)
+            {
+                flashlight.enabled = false;
+                isactive = false;
+                flashlightsource.PlayOneShot(toggleoff);
+            }
+        } 
+    }
+}
