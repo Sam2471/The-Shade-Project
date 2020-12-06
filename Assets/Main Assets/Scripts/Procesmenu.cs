28c39b9be5e8a6315e3596c12606d5cf3314f7aa
Samuel Flowers <475197@chrome.escg.ac.uk>
2020-12-06 00:46:24 +0000

    Set up working main menu

---
+++ Assets/Main Assets/Scripts/Procesmenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Procesmenu : MonoBehaviour
+{
+    public GameObject display;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Countdown());
+        display.SetActive(false);
+    }
+
+    IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds(2);
+        display.SetActive(true);
+        
+    }
+}
