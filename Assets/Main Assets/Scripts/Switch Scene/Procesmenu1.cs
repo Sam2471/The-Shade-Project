5f73f0539415028ec398f82c3afaffcc7d308830
Samuel Flowers <475197@chrome.escg.ac.uk>
2020-12-07 00:34:57 +0000

    Added end scene mouse dosen't work doe

---
+++ Assets/Main Assets/Scripts/Switch Scene/Procesmenu1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Procesmenu1 : MonoBehaviour
+{
+    public GameObject display;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Countdown());
+        display.SetActive(false);
+    }
+
+    IEnumerator Countdown()
+    {
+        yield return new WaitForSeconds(1);
+        display.SetActive(true);
+
+    }
+
+
+}
